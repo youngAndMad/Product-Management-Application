529de829e6f0a1a4d4f5e3d345d02da63bf870b0
daneker <kkraken2005@gmail.com>
2023-03-26 21:08:42 +0600

    angular application

--- src/app/components/create-product/create-product.component.ts
+++ src/app/components/create-product/create-product.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {ProductsService} from "../../services/ProductsService";
-import {products} from "../../data/products";
-import {ModalService} from "../../services/modal.service";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { ProductsService } from "../../services/ProductsService";
+import { ModalService } from "../../services/modal.service";
 
 @Component({
   selector: 'app-create-product',
@@ -10,37 +9,37 @@ import {ModalService} from "../../services/modal.service";
   styleUrls: ['./create-product.component.scss']
 })
 export class CreateProductComponent {
-    form  = new FormGroup({
-      title: new FormControl<string>('' , [
-        Validators.required,
-        Validators.minLength(8)
-      ])
-    })
+  form = new FormGroup({
+    title: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(8)
+    ])
+  })
 
-  constructor( private productService:ProductsService ,
-               private modalService:ModalService
-               ) {
+  constructor(private productService: ProductsService,
+    private modalService: ModalService
+  ) {
 
   }
 
 
-  get title(){
-      return this.form.controls.title as FormControl
+  get title() {
+    return this.form.controls.title as FormControl
   }
 
   submit() {
-   this.productService.create({
-     title: this.form.value.title as string,
-     price: 13.5,
-     description: 'lorem ipsum set',
-     image: 'https://i.pravatar.cc',
-     category: 'electronic',
-     rating:{
-       rate: 44,
-       count:14
-     }
-   }).subscribe(() =>{
-     this.modalService.close();
-   });
+    this.productService.create({
+      title: this.form.value.title as string,
+      price: 13.5,
+      description: 'lorem ipsum set',
+      image: 'https://i.pravatar.cc',
+      category: 'electronic',
+      rating: {
+        rate: 44,
+        count: 14
+      }
+    }).subscribe(() => {
+      this.modalService.close();
+    });
   }
 }
--- src/app/services/ProductsService.ts
+++ src/app/services/ProductsService.ts
@@ -1,43 +1,42 @@
-import {Injectable} from "@angular/core";
-import {HttpClient, HttpErrorResponse, HttpParams} from "@angular/common/http";
-import {catchError, delay, Observable, tap, throwError} from "rxjs";
-import {IProduct} from "../components/models/product";
-import {ErrorService} from "./error.service";
-import {products} from "../data/products";
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse, HttpParams } from "@angular/common/http";
+import { catchError, delay, Observable, tap, throwError } from "rxjs";
+import { IProduct } from "../components/models/product";
+import { ErrorService } from "./error.service";
 
 
 @Injectable({
-  providedIn : 'root'
+  providedIn: 'root'
 })
-export  class ProductsService{
+export class ProductsService {
   constructor(
     private errorService: ErrorService,
     private http: HttpClient
   ) {
   }
 
-  products : IProduct[] = []
+  products: IProduct[] = []
 
-  getAll() : Observable<IProduct[]>{
-    return this.http.get<IProduct[]>('https://fakestoreapi.com/products' , {
-      params : new HttpParams({
-        fromObject: {limit : 25}
+  getAll(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>('https://fakestoreapi.com/products', {
+      params: new HttpParams({
+        fromObject: { limit: 25 }
       })
     }).
-    pipe(
-      delay(200),
-      tap(products => this.products = products),
-      catchError(this.errorHandler.bind(this))
-    );
+      pipe(
+        delay(200),
+        tap(products => this.products = products),
+        catchError(this.errorHandler.bind(this))
+      );
   }
 
-  create(product: IProduct) : Observable<IProduct>{
-    return this.http.post<IProduct>('https://fakestoreapi.com/products' , product).pipe(
+  create(product: IProduct): Observable<IProduct> {
+    return this.http.post<IProduct>('https://fakestoreapi.com/products', product).pipe(
       tap(prod => this.products.push(prod))
     );
   }
 
-  private errorHandler(error : HttpErrorResponse){
+  private errorHandler(error: HttpErrorResponse) {
     this.errorService.handle(error.message);
     return throwError(() => error.message);
   }
